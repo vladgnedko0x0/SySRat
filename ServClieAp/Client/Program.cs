39061e0a13fa524f328670d50e63bd521b078de1
vladgnedko0x0 <vladgnedko0x0@gmail.com>
2024-09-08 13:12:21 +0200

    Update Program.cs

--- ServClieAp/Client/Program.cs
+++ ServClieAp/Client/Program.cs
@@ -173,4 +173,240 @@ class Program
         Console.WriteLine("Functions:");
         PrintFunctionDescription("dir <serverPath>", "Get files and directories at the specified path on the server.");
         PrintFunctionDescription("copy <serverPath>", "Copy a file from the server at the specified path.");
-       
+        PrintFunctionDescription("send <serverPath> <localPath>", "Send a folder or file from the client to the specified path on the server.");
+        PrintFunctionDescription("delete <serverPath>", "Delete a file or folder at the specified path on the server.");
+        PrintFunctionDescription("move <sourcePath> <destinationPath>", "Move a file or folder from the source path to the destination path on the server.");
+        PrintFunctionDescription("run <pathToFileOrExe>", "Run a file or application at the specified path on the server.");
+        PrintFunctionDescription("proc <type(get)>", "Get all processes in alphabetical order on the server.");
+        PrintFunctionDescription("proc <type(kill)> <processID>", "Terminate a process on the server by its ID.");
+        Console.WriteLine();
+
+        Console.WriteLine("Usage:");
+        PrintFunctionUsage(@"dir <serverPath>", @"dir c:\");
+        PrintFunctionUsage(@"copy <serverPath>", @"copy c:\Program Files\Light Shot -- its a test folder");
+        PrintFunctionUsage(@"send <serverPath> <localPath>", @"send c:\ and type a local folder c:\Program Files\My Program");
+        PrintFunctionUsage(@"delete <path>", @"delete c:\Program Files\Test server folder");
+        PrintFunctionUsage(@"move <sourcePath> <destinationPath>", @"move c:\Program Files\TestFolder c:\Program Files\AnotherFolder");
+        PrintFunctionUsage(@"run <pathToFileOrExe>", @"run c:\Program Files\TestProgram\test.exe");
+        PrintFunctionUsage(@"proc <type(get)>", @"proc get");
+        PrintFunctionUsage(@"proc <type(kill)> <processID>", @"proc kill 1234 >> 1234 is a process ID");
+    }
+
+    static void PrintFunctionDescription(string command, string description)
+    {
+        Console.WriteLine($"- {command}: {description}");
+    }
+
+    static void PrintFunctionUsage(string command, string usage)
+    {
+        Console.WriteLine($"- {command}: {usage}");
+    }
+
+    static void SendCommand(NetworkStream stream, string command)
+    {
+        byte[] commandBytes = Encoding.UTF8.GetBytes(command);
+        stream.Write(commandBytes, 0, commandBytes.Length);
+        stream.Flush();
+    }
+
+    static string ReceiveResponse(NetworkStream stream)
+    {
+        byte[] buffer = new byte[20000];
+        int bytesRead = stream.Read(buffer, 0, buffer.Length);
+        return Encoding.UTF8.GetString(buffer, 0, bytesRead);
+    }
+
+    static void ReceiveFileOrDirectory(NetworkStream stream, string currentDirectory = "")
+    {
+        while (true)
+        {
+            byte[] buffer = new byte[8192];
+            int bytesRead = stream.Read(buffer, 0, buffer.Length);
+            string message = Encoding.UTF8.GetString(buffer, 0, bytesRead);
+
+            if (message.StartsWith("Directory"))
+            {
+                string directoryName = message.Split('\\').LastOrDefault();
+                string directoryPath = Path.Combine(currentDirectory, directoryName);
+
+                Directory.CreateDirectory(directoryPath);
+                Console.WriteLine($"Directory created: {directoryPath}");
+                EndProcessMessage(stream, "Directory get ok");
+                ReceiveFileOrDirectory(stream, directoryPath); // Recursively process subfolders
+            }
+            else if (message.StartsWith("End of directory"))
+            {
+                Console.WriteLine(message);
+                EndProcessMessage(stream, "End of directory ok");
+                return; // Return to previous folder after finishing current directory
+            }
+            else
+            {
+                string fileName = message.Split('\'')[1];
+                string filePath = Path.Combine(currentDirectory, fileName);
+                ReceiveFile(stream, filePath);
+            }
+        }
+    }
+
+    static void EndProcessMessage(NetworkStream stream, string processName)
+    {
+        byte[] confirmationMessage = Encoding.UTF8.GetBytes(processName);
+        stream.Write(confirmationMessage, 0, confirmationMessage.Length);
+        stream.Flush();
+    }
+
+    static void ReceiveFile(NetworkStream stream, string defaultFileName)
+    {
+        EndProcessMessage(stream, "FileNameReceived");
+        byte[] fileSizeBuffer = new byte[sizeof(long)];
+        stream.Read(fileSizeBuffer, 0, fileSizeBuffer.Length);
+        long fileSize = BitConverter.ToInt64(fileSizeBuffer, 0);
+        EndProcessMessage(stream, "FileSizeReceived");
+        using (FileStream fileStream = new FileStream(defaultFileName, FileMode.Create))
+        {
+            byte[] buffer = new byte[8192];
+            long totalBytesRead = 0;
+            int bytesRead;
+
+            while (totalBytesRead < fileSize && (bytesRead = stream.Read(buffer, 0, buffer.Length)) > 0)
+            {
+                fileStream.Write(buffer, 0, bytesRead);
+                totalBytesRead += bytesRead;
+            }
+        }
+        // Send confirmation to server that the file was successfully received
+        EndProcessMessage(stream, "FileReceived");
+        Console.WriteLine($"File '{defaultFileName}' received from server.");
+    }
+
+    static string SendFileOrDirectory(string path, NetworkStream stream)
+    {
+        try
+        {
+            if (string.IsNullOrEmpty(path))
+                return "Please provide a file or directory path.";
+
+            if (File.Exists(path))
+            {
+                SendFile(path, stream);
+                return $"File '{Path.GetFileName(path)}' sent.";
+            }
+            else if (Directory.Exists(path))
+            {
+                SendDirectory(path, stream);
+                return $"Directory '{Path.GetFileName(path)}' sent.";
+            }
+            else
+            {
+                return "File or directory not found.";
+            }
+        }
+        catch (Exception ex)
+        {
+            return $"Error sending file or directory: {ex.Message}";
+        }
+        finally
+        {
+            SendResponse(stream, "End of directory");
+        }
+    }
+
+    static void SendDirectory(string directoryPath, NetworkStream stream)
+    {
+        if (directoryPath.Last() != '\\')
+        {
+            SendResponse(stream, $"Directory: {Path.GetDirectoryName(directoryPath + "\\")}");
+            if (!EndProcessMessageGeter(stream, "Directory get ok"))
+            {
+                Console.WriteLine("Directory get error");
+            }
+        }
+        else
+        {
+            SendResponse(stream, $"Directory: {Path.GetDirectoryName(directoryPath)}");
+            if (!EndProcessMessageGeter(stream, "Directory get ok"))
+            {
+                Console.WriteLine("Directory get error");
+            }
+        }
+        // Notify the start of directory content transmission
+
+        // Send files in the directory
+        foreach (string filePath in Directory.GetFiles(directoryPath))
+        {
+            SendFile(filePath, stream);
+        }
+
+        // Recursively send subdirectory contents
+        foreach (string subDirectoryPath in Directory.GetDirectories(directoryPath))
+        {
+            SendDirectory(subDirectoryPath, stream);
+        }
+
+        // Notify the end of directory transmission
+        SendResponse(stream, "End of directory");
+        if (!EndProcessMessageGeter(stream, "End of directory ok"))
+        {
+            Console.WriteLine("End of directory client error");
+        }
+    }
+
+    static bool EndProcessMessageGeter(NetworkStream stream, string processName)
+    {
+        byte[] confirmationBuffer = new byte[8192];
+        int confirmationBytesRead = stream.Read(confirmationBuffer, 0, confirmationBuffer.Length);
+        string confirmationMessage = Encoding.UTF8.GetString(confirmationBuffer, 0, confirmationBytesRead).Trim();
+        if (confirmationMessage == processName)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+        // NEED TO FINISH THIS FUNCTION TO RECEIVE CONFIRMATION FROM CLIENT!!!!!!!!!!!!!!!!!
+    }
+
+    static void SendFile(string filePath, NetworkStream stream)
+    {
+        long fileSize = new FileInfo(filePath).Length;
+        string fileNameMessage = $"File: '{Path.GetFileName(filePath)}'";
+        byte[] fileNameBytes = Encoding.UTF8.GetBytes(fileNameMessage);
+        stream.Write(fileNameBytes, 0, fileNameBytes.Length);
+        stream.Flush();
+
+        // Wait for confirmation from client that the file name was received
+
+        if (!EndProcessMessageGeter(stream, "FileNameReceived"))
+        {
+            Console.WriteLine("Error: File name not received confirmation received.");
+            return;
+        }
+
+        byte[] fileSizeBytes = BitConverter.GetBytes(fileSize);
+        stream.Write(fileSizeBytes, 0, fileSizeBytes.Length);
+        stream.Flush();
+        if (!EndProcessMessageGeter(stream, "FileSizeReceived"))
+        {
+            Console.WriteLine("Error: File size not received confirmation received.");
+            return;
+        }
+        using (var fileStream = File.OpenRead(filePath))
+        {
+            fileStream.CopyTo(stream);
+        }
+        if (!EndProcessMessageGeter(stream, "FileReceived"))
+        {
+            Console.WriteLine("Error: File not received confirmation received.");
+            return;
+        }
+    }
+
+    static void SendResponse(NetworkStream stream, string response)
+    {
+        byte[] responseBytes = Encoding.UTF8.GetBytes(response);
+        stream.Write(responseBytes, 0, responseBytes.Length);
+        stream.Flush();
+    }
+}
